d6c272db5bc601ca032de419052fc70021774b8d
PabloFernandez01 <pablofernandezmoriones@gmail.com>
2022-02-04 12:51:25 +0100

    Editar Datos Usuario, Gestionar Rutas (Item y layout)

---
+++ app/src/main/java/com/mikelalvarez/a4sharedcars/activites/GestionarRutas.java
@@ -0,0 +1,14 @@
+package com.mikelalvarez.a4sharedcars.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionarRutas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionar_rutas);
+    }
+}

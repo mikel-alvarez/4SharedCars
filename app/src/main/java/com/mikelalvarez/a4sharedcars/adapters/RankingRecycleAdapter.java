449468b224e8d85cfe322b0f5862d7144ae6475c
Mikel alvarez <mikelalvarez271@gmail.com>
2022-02-25 08:54:23 +0100

    fallo RankingRecycleAdapter solucionado

--- app/src/main/java/com/mikelalvarez/a4sharedcars/adapters/RankingRecycleAdapter.java
+++ app/src/main/java/com/mikelalvarez/a4sharedcars/adapters/RankingRecycleAdapter.java
@@ -68,7 +68,12 @@ public class RankingRecycleAdapter extends RecyclerView.Adapter<RankingRecycleAd
         public void assingData(Usuario user,OnItemClickListener imgClick){
             lblNombre.setText(user.getNombre());
             lblApellido.setText(user.getApellido());
-            lblPuntos.setText(user.getPuntosC02().toString());
+            if (user.getPuntosC02() == null){
+                lblPuntos.setText("0");
+            } else {
+                lblPuntos.setText(user.getPuntosC02().toString());
+
+            }
             imgAvatar.setImageResource(user.getImagen());
             imgAvatar.setOnClickListener(new View.OnClickListener() {
                 @Override
